96c96c3b543b676596df76036c2f5df9194aa0e4
Justin Tanacredi <jtanacredi@gmail.com>
2015-08-03 15:44:15 -0400

    Stripe - Add address fields to checkout

--- app/client/templates/stripe-checkout/stripe-checkout.js
+++ app/client/templates/stripe-checkout/stripe-checkout.js
@@ -8,6 +8,7 @@
         amount: 5000, // this is equivalent to $50
         name: 'Meteor Tutorial',
         description: 'On how to use Stripe ($50.00)',
+        billingAddress: "true",
         panelLabel: 'Pay Now',
         token: function(res) {
           stripeToken = res.id;
